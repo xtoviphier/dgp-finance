1812f059f904ba3141a911b799782379214dab0d
Christopher William Wambua <gbs_25_1701@stu.edu.ac.ae>
2026-04-22 03:42:23 +0400

    Set Home as default landing page

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="DGP Finance", layout="wide")
 
-st.title("DGP Finance")
-st.markdown("Use the sidebar to navigate through the application.")
+# Redirect instantly to Home
+st.switch_page("Home.py")
